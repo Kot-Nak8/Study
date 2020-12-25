30308d6731c9e413588d050dd469c826981c1111
Kota_Nakamura <66671473+Kota_Nakamrura@users.noreply.github.com>
2020-12-26 00:45:31 +0900

    Realmを用いた基本的なリスト表示

--- kavsoft_realm_test/kavsoft_realm_test/ContentView.swift
+++ kavsoft_realm_test/kavsoft_realm_test/ContentView.swift
@@ -4,8 +4,9 @@
 //
 //  Created by 中村幸太 on 2020/12/25.
 //
-//主キーは設定してない
-//アプリを落として起動させると保存も表示もできない
+//Realmとかを入れるときは「File」の「SwiftPackages」の「Add Package Depen...」から以下のURLを貼り付ける
+//Realmは https://github.com/realm/realm-cocoa
+//
 
 import SwiftUI
 import RealmSwift
@@ -13,72 +14,90 @@ import RealmSwift
 struct ContentView: View {
     @State var name = ""
     @State var age = ""
+    @ObservedObject private var viewModel = get_data()
+    
     var body: some View {
-        VStack{
-        //入力するところ
-            TextField("name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField ("age", text: $age).textFieldStyle(RoundedBorderTextFieldStyle())
-            
-        //入力したのを保存するところ
-            Button(action: {
-                let config = Realm.Configuration(schemaVersion :1)
-                do{
-                    let realm = try Realm(configuration: config)
-                    let newdata = datatype()
-                    newdata.name = self.name
-                    newdata.age = self.age
-                    try realm.write({
-                        realm.add(newdata)
-                        print("success")
-                    })
-                    
-                }
-                catch{
-                    print(error.localizedDescription)
-                }
-                
-            }) {
-                Text("保存")
-            }
-            
-        //デバッグエリアに保存したものを表示する
-            Button(action: {
-                let config = Realm.Configuration(schemaVersion :1)
-                do{
-                    let realm = try Realm(configuration: config)
-                    let result = realm.objects(datatype.self)
-                    //取り出し
-                    print(result[0].age)
-                }
-                catch{
-                    print(error.localizedDescription)
-                }
-                
-            }) {
-                Text("表示")
-            }
-        
-        //データ全削除
-            Button(action: {
-                let config = Realm.Configuration(schemaVersion :1)
-                do{
-                    let realm = try Realm(configuration: config)
-                    let result = realm.objects(datatype.self)
-                    for i in result{
-                        try realm.write({
-                            realm.delete(i)
-                        })
-                    }
-                    print("delete")
-                }
-                catch{
-                    print(error.localizedDescription)
-                }
-                
-            }) {
-                Text("削除")
-            }
-        }.padding()
+        NavigationView{
+            //ホーム画面に表示する
+            List {
+                ForEach(viewModel.itemEntities, id: \.id) { datatype in
+                    Text("\(datatype.name)" + "　" + "\(datatype.age)")
+                }}
+                .navigationBarTitle("ホーム", displayMode: .inline)
+                .navigationBarItems(leading: NavigationLink(destination:
+                //追加ボタンを押すと出てくる画面
+                                VStack{
+                                //入力するところ
+                                    TextField("name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle())
+                                    TextField ("age", text: $age).textFieldStyle(RoundedBorderTextFieldStyle())
+                                    
+                                //入力したのを保存するところ
+                                    Button(action: {
+                                        let config = Realm.Configuration(schemaVersion :1)
+                                        do{
+                                            let realm = try Realm(configuration: config)
+                                            let newdata = datatype()
+                                            //主キーを決定するやつ
+                                            //保存されてるデータの最大のidを取得
+                                            var maxId: Int { return try! Realm().objects(datatype.self).sorted(byKeyPath: "id").last?.id ?? 0 }
+                                            newdata.id = maxId + 1
+                                            newdata.name = self.name
+                                            newdata.age = self.age
+                                            try realm.write({
+                                                realm.add(newdata)
+                                                print("success")
+                                            })
+                                            
+                                        }
+                                        catch{
+                                            print(error.localizedDescription)
+                                        }
+                                        
+                                    }) {
+                                        Text("保存")
+                                    }
+                                    
+                                //デバッグエリアに保存したものを表示する
+                                    Button(action: {
+                                        let config = Realm.Configuration(schemaVersion :1)
+                                        do{
+                                            let realm = try Realm(configuration: config)
+                                            let result = realm.objects(datatype.self)
+                                            //取り出し
+                                            print(result)
+                                        }
+                                        catch{
+                                            print(error.localizedDescription)
+                                        }
+                                        
+                                    }) {
+                                        Text("表示")
+                                    }
+                                
+                                //データ全削除
+                                    Button(action: {
+                                        let config = Realm.Configuration(schemaVersion :1)
+                                        do{
+                                            let realm = try Realm(configuration: config)
+                                            let result = realm.objects(datatype.self)
+                                            for i in result{
+                                                try realm.write({
+                                                    realm.delete(i)
+                                                })
+                                            }
+                                            print("delete")
+                                        }
+                                        catch{
+                                            print(error.localizedDescription)
+                                        }
+                                        
+                                    }) {
+                                        Text("全削除")
+                                    }
+                }.padding(20)){
+                        Text("追加")
+                })//BarItems
+        }
     }
 }
 
@@ -87,11 +106,47 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-class datatype :Object{
     
+
+//RealmのEntity
+class datatype :Object{
+    @objc dynamic var id: Int = 0
     @objc dynamic var name = ""
     @objc dynamic var age = ""
-    
+    private static var config = Realm.Configuration(schemaVersion :1)
+    private static var realm = try! Realm(configuration: config)
+    //idを主キーに設定
+    override static func primaryKey() -> String? {
+            return "id"
+    }
+    //realmのデータを取り出す関数
+    static func all() -> Results<datatype> {
+        realm.objects(datatype.self)
+    }
 }
 
+//データを取得するクラス
+class get_data : ObservableObject{
+    @Published var itemEntities: Results<datatype> = datatype.all()
+    
+    private var notificationTokens: [NotificationToken] = []
+
+    //よくわからんけど下のをコピペしたら更新してくれるようになった
+        init() {
+            // DBに変更があったタイミングでitemEntitiesの変数に値を入れ直す
+            notificationTokens.append(itemEntities.observe { change in
+                switch change {
+                case let .initial(results):
+                    self.itemEntities = results
+                case let .update(results, _, _, _):
+                    self.itemEntities = results
+                case let .error(error):
+                    print(error.localizedDescription)
+                }
+            })
+        }
+
+        deinit {
+            notificationTokens.forEach { $0.invalidate() }
+        }
+}
